403a6d1c2972212bd1d6470de9340aa07c00477a
SujalD04 <sujalrdixit@gmail.com>
2024-09-28 22:05:57 +0530

    Deleted unused files and modified App component

---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+function Login() {
+    return (
+      <div className="flex items-center justify-center h-screen bg-gray-300">
+        <h1 className="text-4xl font-bold text-blue-600">Login Page</h1>
+        {/* You can add a login form here */}
+      </div>
+    );
+  }
+  
+  export default Login;
+  
