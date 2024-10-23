1f0c1830f2e9dbd5d5f6d645b251075def538175
SujalD04 <sujalrdixit@gmail.com>
2024-10-23 16:32:22 +0530

    added some stuff

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -8,15 +8,6 @@ const SignIn = () => {
     <div class="flex justify-center h-screen">
         <div class="hidden bg-cover lg:block lg:w-2/3"  style={{ backgroundImage: "url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')" }}>
             <div class="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
-                <div>
-                    <h2 class="text-2xl font-bold text-white sm:text-3xl">Meraki UI</h2>
-
-                    <p class="max-w-xl mt-3 text-gray-300">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. In
-                        autem ipsa, nulla laboriosam dolores, repellendus perferendis libero suscipit nam temporibus
-                        molestiae
-                    </p>
-                </div>
             </div>
         </div>
 
