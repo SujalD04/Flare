2ad4051890eb3ef71bda001cfde4056738ad0792
SujalD04 <sujalrdixit@gmail.com>
2024-10-23 16:59:04 +0530

    added a page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import SignIn from './pages/signIn';
 import SignUp from './pages/signUp';
+import Homepage from './pages/Homepage';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/" element={<Navigate to="/sign-in" />} /> 
+        <Route path="/Homepage" element={<Homepage />} />
       </Routes>
     </Router>
   );
